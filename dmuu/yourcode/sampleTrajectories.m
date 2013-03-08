acbda36d77e9b320b1a4bab646658588b238ae5c
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2013-03-08 16:27:12 +0100

    Deel 1 & 2 deels werkend. Nu nog voor sparse matrixen

--- dmuu/yourcode/sampleTrajectories.m
+++ dmuu/yourcode/sampleTrajectories.m
@@ -1,5 +1,29 @@
-function sampleTrajectories(Q)
+function states = sampleTrajectories(Q)
 % sample a number of trajectories through the MDP, following the
 % policy specified by Q.
+global problem;
+
+% Select a start state
+startState = min(find(cumsum(problem.start)>rand));
+
+state = startState;
+states = [state];
+actions = [];
+for x = 1:20
+    % Select next action
+    nextAction = getActionForState(Q,state);
+    
+    nextState = sampleSuccessorState(state, nextAction);
+    states = [states, nextState];
+    actions = [actions, nextAction];
+    
+    state = nextState;
+end
+plotSequence(states);
+states
+actions
+
+
+
 
 
