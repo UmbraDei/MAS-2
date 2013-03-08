ef7b4554743e9feb0be2ae46e153a61d2e181771
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2013-03-08 14:15:16 +0100

    Opgave deel 1

--- dmuu/yourcode/vi.m
+++ dmuu/yourcode/vi.m
@@ -8,3 +8,31 @@ terminalStates=getTerminalStates;
 convergenceThreshold=1e-6;
 
 Q=zeros(problem.nrStates,problem.nrActions);
+eps = 0.0001;
+
+delta = eps +1;
+
+while delta > eps
+    QNew=zeros(problem.nrStates,problem.nrActions);
+    delta = 0;
+    for state=1:problem.nrStates
+       for action=1:problem.nrActions
+           
+           v = Q(state, action);
+           
+           tempSum = 0;
+           for nextState = 1: problem.nrStates
+               tempSum = tempSum  + ...
+                    problem.transition(nextState, state, action)* max(Q(nextState,:));
+           end
+           
+           QNew(state, action) = problem.reward(state,action) + problem.gamma*tempSum;
+           
+           delta = max(delta, abs(v-QNew(state, action)));
+       end
+    end
+    Q = QNew;
+end
+
+
+
