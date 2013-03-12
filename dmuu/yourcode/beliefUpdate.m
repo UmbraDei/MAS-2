0dbbba51a60581f4b5188f7d5faf36eda0cad2ea
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2013-03-12 15:53:56 +0100

    beliefUpdate.m geimplementeerd

---
+++ dmuu/yourcode/beliefUpdate.m
@@ -0,0 +1,15 @@
+function b1 = beliefUpdate(b,a,o)
+% Update belief b to b1 taking action a (from b) and receiving observation o
+% With b the vector of the beliefs
+
+global problem;
+
+b1=zeros(1,problem.nrStates);
+
+transitionMatrix = getTransisitionMatrix(a);
+
+b1 = o .* (transitionMatrix*b);
+end
+
+
+
