b6ddcb1a29eb42af8f1415b3a472d253bcc91c60
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2013-03-19 16:40:12 +0100

    Sample trajectories updated

--- dmuu/yourcode_new/sampleTrajectories.m
+++ dmuu/yourcode_new/sampleTrajectories.m
@@ -1,5 +1,52 @@
 function sampleTrajectories(Q)
 % sample a number of trajectories through the MDP, following the
 % policy specified by Q.
+    global problem;
+    nbOfIterations = 50;
+
+    %% Toekennen van startpositie
+    state = find(cumsum(problem.start)>rand, 1 );
+
+    %% Het tekenen
+    plotState(state);
+    waitforbuttonpress;
+
+    %% 
+    states = [state];
+    actions = [];
+
+    %%
+    for i = 1:nbOfIterations
+        %% Volgende actie
+        action = getActionForState(Q, state);
+
+        %% Volgende state
+        nextState = sampleSuccessorState(state, action);
+
+        %% Het tekenen
+        waitforbuttonpress;
+        plotState(nextState);
+
+        %%Updaten van de state
+        state = nextState;
+
+        %%
+        states = [states, state];
+        actions = [actions, action];
+    end
+
+function s = sampleSuccessorState(state, action)
+    %Sample a successor state s2 (at t+1) given state s and action a at t
+    
+    global problem;
+    
+    if problem.useSparse == 0
+        s = min(find(cumsum(problem.transition(:,state, action))>rand));
+    else
+        s = min(find(cumsum(problem.transitionS{action}(:,state))>rand));
+    end
+
+
+
 
 
